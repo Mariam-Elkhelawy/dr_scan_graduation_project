94c9de74f03d181d18f98e5d5b3d7bd2e150a23a
Mariam-Elkhelawy <112530709+Mariam-Elkhelawy@users.noreply.github.com>
2023-05-21 23:22:03 +0300

    camera

--- lib/pages/madicalanalysis_page.dart
+++ lib/pages/madicalanalysis_page.dart
@@ -1,15 +1,25 @@
+import 'dart:io';
 import 'dart:ui';
 import 'package:dr_scan_graduation_project/constants.dart';
 import 'package:dr_scan_graduation_project/widgets/custom_button.dart';
 import 'package:dr_scan_graduation_project/widgets/nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:text_divider/text_divider.dart';
 
-class MedicalAnalysisPage extends StatelessWidget {
+import 'loading_page.dart';
+
+class MedicalAnalysisPage extends StatefulWidget {
   const MedicalAnalysisPage({Key? key}) : super(key: key);
   static String id = 'MedicalAnalysisPage';
 
+  @override
+  State<MedicalAnalysisPage> createState() => _MedicalAnalysisPageState();
+}
+
+class _MedicalAnalysisPageState extends State<MedicalAnalysisPage> {
+  File? imageFile;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,7 +101,8 @@ class MedicalAnalysisPage extends StatelessWidget {
                       return BackdropFilter(
                         filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
                         child: AlertDialog(
-                            insetPadding: EdgeInsets.symmetric(vertical: 94, horizontal: 10),
+                            insetPadding: EdgeInsets.symmetric(
+                                vertical: 94, horizontal: 10),
                             shape: RoundedRectangleBorder(
                                 borderRadius:
                                     BorderRadius.all(Radius.circular(12))),
@@ -121,6 +132,16 @@ class MedicalAnalysisPage extends StatelessWidget {
                               children: [
                                 Image.asset('assets/images/medical_scan.png'),
                                 Button(
+                                  ontap: () {
+                                    getImage(source: ImageSource.camera);
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        context = context;
+                                        return const LoadingPage();
+                                      },
+                                    );
+                                  },
                                   icon: Icon(Icons.camera_alt_outlined),
                                   iconText: 'Use camera',
                                   width: 54,
@@ -145,6 +166,8 @@ class MedicalAnalysisPage extends StatelessWidget {
                                   height: 30,
                                 ),
                                 Button(
+                                  ontap: () =>
+                                      getImage(source: ImageSource.gallery),
                                   icon: Icon(FontAwesomeIcons.filePdf),
                                   iconText: 'Upload file / photo',
                                   width: 35,
@@ -169,54 +192,79 @@ class MedicalAnalysisPage extends StatelessWidget {
           ],
         ),
       ]),
-      bottomNavigationBar: NavBar(istabbed3: true,istabbed1: false,istabbed: false,istabbed2: false,),
+      bottomNavigationBar: NavBar(
+        istabbed1: false,
+        istabbed2: false,
+        istabbed: false,
+        istabbed3: true,
+      ),
     );
+  }
+
+  void getImage({required ImageSource source}) async {
+    final file = await ImagePicker().pickImage(
+      source: source,
+    );
+
+    if (file?.path != null) {
+      setState(() {
+        imageFile = File(file!.path);
+      });
+    }
   }
 }
 
 class Button extends StatelessWidget {
-  Button({required this.icon, required this.iconText,required this.width});
+  Button(
+      {required this.icon,
+      required this.iconText,
+      required this.width,
+      this.ontap});
   Widget icon;
   String iconText;
   double width;
+  var ontap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      height: 45,
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 15),
-            child: IconButton(
-              onPressed: () {},
-              icon: icon,
-              color: kWhiteColor,
+    return GestureDetector(
+      onTap: ontap,
+      child: Container(
+        width: 300,
+        height: 45,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: IconButton(
+                onPressed: () {},
+                icon: icon,
+                color: kWhiteColor,
+              ),
             ),
-          ),
-          SizedBox(
-            width: width,
-          ),
-          Text(
-            iconText,
-            style: TextStyle(
-                fontSize: 16,
-                fontFamily: kRoboto,
-                fontWeight: FontWeight.w600,
-                color: kWhiteColor),
-          ),
-        ],
-      ),
-      decoration: BoxDecoration(
-        color: kPrimaryColor,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(.5),
-            blurRadius: 13,
-            offset: Offset(0, 1),
-          )
-        ],
-        borderRadius: BorderRadius.circular(15),
+            SizedBox(
+              width: width,
+            ),
+            Text(
+              iconText,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: kRoboto,
+                  fontWeight: FontWeight.w600,
+                  color: kWhiteColor),
+            ),
+          ],
+        ),
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(.5),
+              blurRadius: 13,
+              offset: Offset(0, 1),
+            )
+          ],
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
